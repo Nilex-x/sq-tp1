b2686a9c184709bc58480b30b94bdcbef2829c3e
Nilexx <nicolas.moreau@epitech.eu>
2024-02-12 18:19:47 -0500

    My site ready for deployment.

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,20 +1,15 @@
 import {initializeApp} from "firebase/app";
 import {getDatabase} from "firebase/database";
-import dotenv from "dotenv"
-import dotenvExpand from 'dotenv-expand'
-
-// @ts-ignore
-dotenvExpand(dotenv.config());
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.VITE_API_KEY,
-    authDomain: process.env.VITE_AUTH_DOMAIN,
-    projectId: process.env.VITE_PROJECT_ID,
-    storageBucket: process.env.VITE_STORAGE_BUCKET,
-    messagingSenderId: process.env.VITE_SENDER_ID,
-    appId: process.env.VITE_APP_ID,
-    databaseUrl: process.env.VITE_DB_URL
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
+    databaseUrl: import.meta.env.VITE_DB_URL
 };
 
 // Initialize Firebase
